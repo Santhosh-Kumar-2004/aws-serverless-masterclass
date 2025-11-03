4c9a100f418e808f3652070a0cf4c95b7c169540
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-11-03 10:50:21 +0530

    Created the basic post endpoint

--- lambda-func-essentials/endpoints.py
+++ lambda-func-essentials/endpoints.py
@@ -5,3 +5,7 @@ router = APIRouter(prefix="/data")
 @router.get("")
 def get_data():
     return {"data": "Data retrieved successfully!"}
+
+@router.post()
+def post_data():
+    return {"data": "Data created successfully!"}
